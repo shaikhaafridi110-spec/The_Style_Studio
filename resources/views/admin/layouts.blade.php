7b915646f57db4369afc04f48ced03d1ba0d186c
shaikhaafridi110-spec <shaikhaafridi110@gmail.com>
2026-02-21 23:52:07 +0530

    ..

--- resources/views/admin/layouts.blade.php
+++ resources/views/admin/layouts.blade.php
@@ -90,7 +90,7 @@
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
                 <!-- partial -->
-                <footer class="footer" style="">
+                <footer class="footer" >
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
                         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024 <a
                                 href="https://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
